77324ec67670637e3b5af3d401ffb415d54583bb
makoto2222 <lets_talk_about_feeling@yahoo.co.jp>
2020-10-27 20:31:06 +0900

    PHP応用課題Q17~Q21まで終了

--- task.php
+++ task.php
@@ -1,174 +1,192 @@
+# 課題の回答は このファイル をご利用下さい。
+# 回答の出力を確認される際は，「php task.php」をターミナルから実行して下さい。
+
+
 <?php
 
+//以下Q17---------------------------------------
 
-print("#####q9#####".PHP_EOL);
-$names = ["田中", "佐藤", "佐々木", "高橋"];
+class User
+{
 
-    # 以下に回答を記載
+    protected $name;
+    protected $age;
+    protected $gender;
 
-$name = [];
-
-foreach ($names as $key => $value ) {
-    $number = $key + 1;
-    $names1= '会員No.' . $number . " " . $value;
-
-    array_push($name, $names1);
-}
-
-print_r($name);
-echo PHP_EOL;
-
-
-
-
-
-
-print("#####q10#####".PHP_EOL);
-$foods = ["いか","たこ","うに","しゃけ","うにぎり","うに軍艦","うに丼"];
-
-    # 以下に回答を記載
-
-foreach ($foods as $food) {
-
-    if (preg_match("/うに/", $food)) {
-        echo '好物です';
-        echo PHP_EOL;
-
-    } else {
-        echo 'まぁまぁ好きです';
+    function __construct($name, $age, $gender) {
+        $this->name = $name;
+        $this->age = $age;
+        $this->gender = $gender;
     }
+
+    function info() {
+        print("名前:".$this->name.PHP_EOL);
+        print("年齢:".$this->age.PHP_EOL);
+        print("性別:".$this->gender.PHP_EOL);
+    }
+
 }
+
+$user1 = new User("神里",32,"男");
+$user2 = new User("あじー",32,"男");
+
+$user1->info();
+print("-------------".PHP_EOL);
+$user2->info();
+
 echo PHP_EOL;
 
 
 
 
 
+//以下Q18---------------------------------------
 
-print("#####q11#####".PHP_EOL);
-$sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]];
+print("#####q18#####".PHP_EOL);
 
-  # 以下に回答を記載
+class Man
+{
 
+  protected $name;
+  protected $age;
 
-  $sports1 = [];
+  function __construct($name, $age) {
+    $this->name = $name;
+    $this->age = $age;
+}
 
-  foreach($sports as $key => $sport){
-      if(is_array($sport)){
-          $sports1 = array_merge($sports1,$sport);
-      }else{
-          array_push($sports1,$sport);
-      }
-  }
-
-  $sports2 = array_unique($sports1);
-  $sports3 = array_values($sports2);
-
-  $sports4 = [];
-
-  foreach($sports3 as $key => $sport3){
-      $number = $key + 1;
-      $sport4 = "No" . $number ." " . $sport3;
-      array_push($sports4, $sport4);
-  }
-
-  echo "ユーザーの趣味一覧";
-  echo PHP_EOL;
-
-  foreach($sports4 as $sport4){
-      print_r($sport4);
+  function introduce(){
+    if($this->age >20){
+      print("こんにちは，" . $this->name . "と申します。宜しくお願いいたします。");
       echo PHP_EOL;
+    } else {
+      print("はいさいまいど〜，" . $this->name .  "です！！！");
+    }
+  }
+}
+
+$man1 = new Man("あじー",32);
+$man2 = new Man("ゆたぼん",10);
+
+$man1->introduce();
+$man2->introduce();
+
+echo PHP_EOL;
+
+
+
+//以下Q19---------------------------------------
+
+
+print("#####q19#####".PHP_EOL);
+class Item
+{
+  # 以下を修正して下さい
+
+  public $name;
+
+  function __construct($book_name){
+    $this->name = $book_name;
+  }
+}
+  # 以下は変更しないで下さい
+
+$book = new Item("ゼロ秒思考");
+print($book->name.PHP_EOL);
+
+echo PHP_EOL;
+
+
+
+//以下Q20---------------------------------------
+
+
+print("#####q20#####".PHP_EOL);
+
+
+class Human
+{
+
+  public $name;
+  public $age;
+
+  function __construct($user_name, $user_age) {
+    $this->name = $user_name;
+    $this->age = $user_age;
   }
 
-
-
-
-
-print("#####q12#####".PHP_EOL);
-$data = [ "user" => [ "name" => "satou", "age" => 33 ] ];
-
-  # 以下に回答を記載
-
-echo $data["user"]["name"];
-echo PHP_EOL;
-
-
-
-
-
-
-print("#####q13#####".PHP_EOL);
-$user_data = [ "name" => "神里", "age" => 31, "address" => "埼玉"];
-$update_data = [ "age" => 32, "address" => "沖縄" ];
-
-
-  # 以下に回答を記載
-
-
-$user_data = array_replace( $user_data, $update_data ) ;
-print_r($user_data);
-echo PHP_EOL;
-
-
-
-
-
-
-print("#####q14#####".PHP_EOL);
-$data = [ "name" => "satou", "age" => 33, "address" => "saitama", "hobby" => "soccer", "email" => "hoge@fuga.com" ];
-
-  # 以下に回答を記載
-
-print_r(array_values($data));
-echo PHP_EOL;
-
-
-
-
-
-
-print("#####q15#####".PHP_EOL);
-$data1 = [ "name" => "saitou", "hobby" => "soccer", "age" => 33, "role" => "admin" ];
-$data2 = [ "name" => "yamada", "hobby" => "baseball", "role" => "normal" ];
-
-  # 以下に回答を記載
-
-
-if (array_key_exists('age', $data1) ) {
-  echo "OK";
-  echo PHP_EOL;
-}else {
-  echo "NG";
-  echo PHP_EOL;
 }
 
-if (array_key_exists('age', $data2) ) {
-  echo "OK";
-  echo PHP_EOL;
-}else {
-  echo "NG";
+class Zoo
+{
+
+  protected $name;
+  protected $entry_fee;
+
+  function __construct($zoo_name, $zoo_entry_fee) {
+    $this->name = $zoo_name;
+    $this->entry_fee = $zoo_entry_fee;
+  }
+
+  function info_entry_fee(Human $human) {
+      if($human->age <= 5){
+        print($human->name."さんの入場料金は ".$this->entry_fee["infant"]." 円です。".PHP_EOL);
+      }elseif($human->age <= 12){
+        print($human->name."さんの入場料金は ".$this->entry_fee["children"]." 円です。".PHP_EOL);
+      }elseif($human->age <= 64){
+        print($human->name."さんの入場料金は ".$this->entry_fee["adult"]." 円です。".PHP_EOL);
+      }elseif($human->age <= 120){
+        print($human->name."さんの入場料金は ".$this->entry_fee["senior"]." 円です。".PHP_EOL);
+      }
+
+    }
+
 }
-echo PHP_EOL;
+
+
+ $zoo = new Zoo("旭山動物園",[ "infant" => 0, "children" => 400, "adult" => 800, "senior" => 500]);
+
+ $human1 = new Human("たま",3);
+ $human2 = new Human("ゆたぼん",10);
+ $human3 = new Human("あじー",32);
+ $human4 = new Human("ぎん",108);
+
+ $humans = [ $human1, $human2, $human3, $human4 ];
+
+ foreach($humans as $human){
+   $zoo->info_entry_fee($human);
+ }
+
+ echo PHP_EOL;
 
 
 
 
 
 
-print("#####q16#####".PHP_EOL);
-$users = [
-  [ "name" => "satou", "age" => 22 ],
-  [ "name" => "yamada", "age" => 12 ],
-  [ "name" => "takahashi", "age" => 32 ],
-  [ "name" => "nakamura", "age" => 41 ]
-];
+//以下Q21---------------------------------------
 
-  # 以下に回答を記載
+print("#####q21#####".PHP_EOL);
 
-  foreach($users as $user){
-    print_r("私の名前は{$user['name']}です。年齢は{$user['age']}歳です。");
+for ($i = 1 ; $i <= 30 ; $i++) {
+
+  if($i % 3 === 0 & $i % 5 ===0){
+    echo 'FizzBuzz';
     echo PHP_EOL;
+  } elseif($i % 3 === 0 & $i % 7 ===0){
+    echo 'FizzHoge';
+    echo PHP_EOL;
+  } elseif($i % 7 ===0){
+    echo 'Hoge';
+    echo PHP_EOL;
+  } elseif($i % 5===0){
+    echo 'Buzz';
+    echo PHP_EOL;
+  } elseif($i % 3===0){
+    echo 'Fizz';
+    echo PHP_EOL;
+  } else {
+    echo $i;
+    echo PHP_EOL;
+  }
 }
-
-
-?>
